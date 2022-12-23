8a46445113739335284fb24d081308fb2012910b
jaimeAbrilmintic2022 <jaimecursohenry@gmail.com>
2022-12-22 19:18:40 -0500

    configuracion de backend para despliegue

--- Backend/app.js
+++ Backend/app.js
@@ -7,7 +7,7 @@ import messageRouter from "./routes/messageRouter.js";
 import motoRouter from "./routes/motoRouter.js";
 import reservationRouter from "./routes/reservationRouter.js";
 import userRouter from "./routes/userRouter.js";
-import { API_URL } from "./apiBacken.js";
+
 mongoose.set('strictQuery', false);
 
 //const cors = require('cors');
@@ -31,7 +31,7 @@ mongoose.connect("mongodb+srv://motorcycleapp:motorcycleapp@clustermotorcycle.5u
 //Middlware
 
 app.use(cors({
-    origin: API_URL
+    origin: process.env.APP_URL
 }))
 app.use(express.json())
 app.use("/user",userRouter)
